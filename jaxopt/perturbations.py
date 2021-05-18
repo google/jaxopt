17de4465852ecc557d78dc9b02284107d5cc054b
Quentin Berthet <qberthet@google.com>
2021-05-18 10:33:36 +0000

    Perturbed max from argmax in JAXOPT.
    This operator creates a perturbed max function from an argmax function.
    
    PiperOrigin-RevId: 374380853

--- jaxopt/perturbations.py
+++ jaxopt/perturbations.py
@@ -17,8 +17,6 @@ from typing import Tuple, Callable
 import jax
 import jax.numpy as jnp
 
-Sampler = Callable[[jnp.array], Tuple[jnp.array]]
-
 
 class Normal:
   """Normal distribution."""
@@ -69,7 +67,7 @@ def make_perturbed_argmax(argmax_fun: Callable[[jnp.array], jnp.array],
       def argmax_fun(x):
         return jax.nn.one_hot(jnp.argmax(x), x.shape[0])
 
-      pert_argmax_fun = make_perturbed_argmax(argmax_fn,
+      pert_argmax_fun = make_perturbed_argmax(argmax_fun,
                                               num_samples=200,
                                               sigma=0.01)
 
@@ -107,6 +105,74 @@ def make_perturbed_argmax(argmax_fun: Callable[[jnp.array], jnp.array],
         jnp.reshape(nabla_z_flat, (num_samples, -1)),
         tangent)
     return jnp.reshape(tangent_flat, inputs.shape)
+
+  forward_pert.defjvps(pert_jvp, None)
+
+  return forward_pert
+
+
+def make_perturbed_max(argmax_fun: Callable[[jnp.array], jnp.array],
+                       num_samples: int = 1000,
+                       sigma: float = 0.1,
+                       noise=Gumbel()):
+  """Turns an argmax in a differentiable version of the max with perturbations.
+
+  Args:
+    argmax_fun: the argmax function to transform into a differentiable version.
+      The signature for argmax_fn currently supported for custom jvp and jit is:
+      + input [D1, ..., Dk], output [D1, ..., Dk], k >= 1
+    num_samples: an int, the number of perturbed outputs to average over.
+    sigma: a float, the scale of the random perturbation.
+    noise: a distribution object that must implement a sample function and a
+      log-pdf of the desired distribution, similar to the examples above.
+      Default is Gumbel distribution.
+
+  Returns:
+    A function with the same inputs (and an rng) that can be differentiated.
+
+  Example:
+    Given an argmax function, e.g.
+
+      def argmax_fun(x):
+        return jax.nn.one_hot(jnp.argmax(x), x.shape[0])
+
+      pert_max_fun = make_perturbed_max(argmax_fun,
+                                        num_samples=200,
+                                        sigma=0.01)
+
+    Then pert_max_fun is differentiable, a perturbed version of the associated
+    max to argmax_fun. Since it relies on randomness, it requires an rng key
+
+      pert_max = pert_max_fun(x, rng)
+
+    When handling a batched input, vmap can be applied to this function, with
+    some care in splitting the rng key.
+
+      batch_size = x_batch.shape[0]
+      rngs_batch = jax.random.split(rng, batch_size)
+      pert_max = jax.vmap(pert_max_fun)(x_batch, rngs_batch)
+
+    Further, if the argmax_fun is jittable, then so is pert_max_fun.
+  """
+
+  @jax.custom_jvp
+  def forward_pert(inputs, rng):
+    samples = noise.sample(seed=rng,
+                           sample_shape=(num_samples,) + inputs.shape)
+    output_pert = jax.vmap(argmax_fun)(inputs + sigma * samples)
+    max_values = jnp.einsum('nd,nd->n',
+                            jnp.reshape(inputs + sigma * samples,
+                                        (num_samples, -1)),
+                            jnp.reshape(output_pert, (num_samples, -1)))
+    return jnp.mean(max_values)
+
+  def pert_jvp(tangent, _, inputs, rng):
+    pert_argmax_fun = make_perturbed_argmax(argmax_fun,
+                                            num_samples,
+                                            sigma,
+                                            noise)
+    pert_argmax = pert_argmax_fun(inputs, rng)
+    return jnp.sum(pert_argmax * tangent)
 
   forward_pert.defjvps(pert_jvp, None)
 
--- tests/perturbations_test.py
+++ tests/perturbations_test.py
@@ -24,7 +24,7 @@ from jaxopt import perturbations
 
 
 def one_hot_argmax(inputs: jnp.array) -> jnp.array:
-  """An argmax one-hot functions for arbitrary shapes."""
+  """An argmax one-hot function for arbitrary shapes."""
   inputs_flat = jnp.reshape(inputs, (-1))
   flat_one_hot = jax.nn.one_hot(jnp.argmax(inputs_flat), inputs_flat.shape[0])
   return jnp.reshape(flat_one_hot, inputs.shape)
@@ -34,7 +34,7 @@ def ranks(inputs: jnp.array) -> jnp.array:
   return jnp.argsort(jnp.argsort(inputs, axis=-1), axis=-1)
 
 
-class PerturbationsTest(jtu.JaxTestCase):
+class PerturbationsArgmaxTest(jtu.JaxTestCase):
 
   def setUp(self):
     super().setUp()
@@ -183,13 +183,13 @@ class PerturbationsTest(jtu.JaxTestCase):
     theta = jnp.array([-0.8, 0.6, 1.2, -1.0, -0.7, 0.6, 1.1, -1.0, 0.4])
     # High value of num_samples for this specific test. Not required in normal
     # usecases, as in learning tasks.
-    pert_ranks_fn = jax.jit(perturbations.make_perturbed_argmax(
+    pert_ranks_fun = jax.jit(perturbations.make_perturbed_argmax(
         argmax_fun=ranks,
         num_samples=100000,
         sigma=self.sigma,
         noise=perturbations.Normal()))
 
-    square_loss_rank = lambda x, rng: jnp.mean(pert_ranks_fn(x, rng) ** 2)
+    square_loss_rank = lambda x, rng: jnp.mean(pert_ranks_fun(x, rng) ** 2)
 
     gradient_square_rank = jax.grad(square_loss_rank)(theta, self.rng)
     eps = 1e-2
@@ -201,6 +201,143 @@ class PerturbationsTest(jtu.JaxTestCase):
     delta_lin = jnp.sum(gradient_square_rank * h)
 
     self.assertArraysAllClose(delta_num, delta_lin, atol=5e-2)
+
+
+class PerturbationsMaxTest(jtu.JaxTestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.num_samples = 1000
+    self.sigma = 0.5
+    self.rng = jax.random.PRNGKey(0)
+    self.theta_batch = jnp.array([[-0.6, 1.9, -0.2, 1.7, -1.0],
+                                  [-0.6, 1.0, -0.2, 1.8, -1.0]],
+                                 dtype=jnp.float32)
+
+  def test_max_small_sigma(self):
+    """Checks that the perturbed argmax is close to the max for small sigma."""
+    pert_max_small_sigma_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=one_hot_argmax,
+        num_samples=self.num_samples,
+        sigma=1e-7,
+        noise=perturbations.Gumbel()))
+    rngs_batch = jax.random.split(self.rng, 2)
+    pert_max_small_sigma = jax.vmap(pert_max_small_sigma_fun)(self.theta_batch,
+                                                              rngs_batch)
+
+    self.assertArraysAllClose(pert_max_small_sigma,
+                              jnp.array([1.9, 1.8]), atol=1e-6)
+
+  def test_max_jensen(self):
+    """Checks that the noise increases the value of the expected max."""
+    rngs_batch = jax.random.split(self.rng, 2)
+    pert_max_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=one_hot_argmax,
+        num_samples=10000,
+        sigma=self.sigma,
+        noise=perturbations.Gumbel()))
+    pert_max = jax.vmap(pert_max_fun)(self.theta_batch, rngs_batch)
+    self.assertGreater(pert_max[0], 1.9)
+    self.assertGreater(pert_max[1], 1.8)
+
+  def test_grads(self):
+    """Tests that the perturbed and regularized maxes match."""
+    pert_max_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=one_hot_argmax,
+        num_samples=10000,
+        sigma=self.sigma,
+        noise=perturbations.Gumbel()))
+
+    def sum_max(theta_batch, rng):
+      batch_size = theta_batch.shape[0]
+      rngs_batch = jax.random.split(rng, batch_size)
+      pert_max_batch = jax.vmap(pert_max_fun)(theta_batch, rngs_batch)
+      return jnp.sum(pert_max_batch)
+
+    soft_argmax = jax.nn.softmax(self.theta_batch/self.sigma)
+
+    grad_pert = jax.grad(sum_max)(self.theta_batch, self.rng)
+
+    self.assertArraysAllClose(soft_argmax, grad_pert, atol=1e-2)
+
+  def test_noise_iid(self):
+    """Checks that different elements of the batch have different noises."""
+    pert_max_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=one_hot_argmax,
+        num_samples=10000,
+        sigma=self.sigma,
+        noise=perturbations.Gumbel()))
+    theta_batch_repeat = jnp.array([[-0.6, 1.9, -0.2, 1.1, -1.0],
+                                    [-0.6, 1.9, -0.2, 1.1, -1.0]],
+                                   dtype=jnp.float32)
+    rngs_batch = jax.random.split(self.rng, 2)
+    pert_max_repeat = jax.vmap(pert_max_fun)(theta_batch_repeat, rngs_batch)
+    self.assertArraysAllClose(pert_max_repeat[0], pert_max_repeat[1],
+                              atol=1e-2)
+    delta_noise = pert_max_repeat[0] - pert_max_repeat[1]
+    self.assertNotAlmostEqual(jnp.linalg.norm(delta_noise), 0)
+
+  def test_distrax(self):
+    """Checks that the function is compatible with distrax distributions."""
+
+    theta_batch = jnp.array([[-0.5, 0.2, 0.1],
+                             [-0.2, 0.1, 0.4]])
+    pert_max_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=one_hot_argmax,
+        num_samples=self.num_samples,
+        sigma=self.sigma,
+        noise=perturbations.Normal()))
+    rngs_batch = jax.random.split(self.rng, 2)
+    pert_max = jax.vmap(pert_max_fun)(theta_batch, rngs_batch)
+
+    dist_max_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=one_hot_argmax,
+        num_samples=self.num_samples,
+        sigma=self.sigma,
+        noise=distrax.Normal(loc=0., scale=1.)))
+    dist_max = jax.vmap(dist_max_fun)(theta_batch, rngs_batch)
+
+    self.assertArraysAllClose(pert_max, dist_max, atol=1e-6)
+
+  def test_rank_finite_diff(self):
+    theta = jnp.array([-0.8, 0.6, 1.2, -1.0, -0.7, 0.6, 1.1, -1.0, 0.4])
+    # High value of num_samples for this specific test. Not required in normal
+    # usecases, as in learning tasks.
+    pert_ranks_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=ranks,
+        num_samples=100000,
+        sigma=self.sigma,
+        noise=perturbations.Normal()))
+
+    gradient_ranks_value = jax.grad(pert_ranks_fun)(theta, self.rng)
+    eps = 1e-2
+    h = jax.random.uniform(self.rng, shape=theta.shape)
+    rngs = jax.random.split(self.rng, 2)
+    delta_num = (pert_ranks_fun(theta + eps * h, rngs[0]) -
+                 pert_ranks_fun(theta - eps * h, rngs[0])) / (2 * eps)
+    delta_lin = jnp.sum(gradient_ranks_value * h)
+
+    self.assertArraysAllClose(delta_num, delta_lin, rtol=1e-2)
+
+  def test_rank_autodiff(self):
+    rnd_b_s = 10
+    theta_random_batch = jax.random.uniform(self.rng, (rnd_b_s, 5))
+    pert_ranks_max_fun = jax.jit(perturbations.make_perturbed_max(
+        argmax_fun=ranks,
+        num_samples=self.num_samples,
+        sigma=self.sigma,
+        noise=perturbations.Normal()))
+    pert_ranks_argmax_fun = jax.jit(perturbations.make_perturbed_argmax(
+        argmax_fun=ranks,
+        num_samples=self.num_samples,
+        sigma=self.sigma,
+        noise=perturbations.Normal()))
+    rngs = jax.random.split(self.rng, rnd_b_s)
+    autodiff_grad = jax.vmap(jax.grad(pert_ranks_max_fun))(theta_random_batch,
+                                                           rngs)
+    expected_grad = jax.vmap(pert_ranks_argmax_fun)(theta_random_batch, rngs)
+    self.assertArraysAllClose(autodiff_grad, expected_grad, rtol=1e-6)
+
 
 if __name__ == '__main__':
   absltest.main(testLoader=jtu.JaxTestLoader())
