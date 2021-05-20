f36967e0141e906fd60d279d198bf89ce9e61536
Mathieu Blondel <mblondel@google.com>
2021-05-20 19:41:26 +0000

    Add various projection operators.
    PiperOrigin-RevId: 374929173

--- jaxopt/projection.py
+++ jaxopt/projection.py
@@ -15,12 +15,46 @@
 """Projection operators."""
 
 from typing import Any
+from typing import Tuple
 
 import jax
 import jax.numpy as jnp
 from jaxopt import tree_util
 
+from jaxopt import quadratic_prog
 from jaxopt import root_finding
+
+
+def projection_non_negative(x: Any, params=None) -> Any:
+  r"""Projection onto the non-negative orthant.
+
+  The output is ``argmin_{p >= 0} ||x - p||``
+
+  Args:
+    x: pytree to project.
+    params: ignored.
+  Returns:
+    p: projected pytree, same structure as ``x``.
+  """
+  return tree_util.tree_map(jax.nn.relu, x)
+
+
+def projection_box(x: Any, params: Tuple) -> Any:
+  r"""Projection onto box constraints.
+
+  The output is ``argmin_{lower <= p <= upper} ||x - p||``
+
+  where ``(lower, upper) = params``.
+
+  Args:
+    x: pytree to project.
+    params: a tuple ``(lower, upper)``. ``lower`` and ``upper`` can be either
+      scalar values or pytrees of the same structure as ``x``.
+  Returns:
+    p: projected pytree, same structure as ``x``.
+  """
+  lower, upper = params
+  return tree_util.tree_multimap(jnp.clip, x, lower, upper)
 
 
 @jax.custom_jvp
@@ -62,23 +96,177 @@ def projection_simplex(x: jnp.ndarray, s: float = 1.0) -> jnp.ndarray:
   return s * _projection_unit_simplex(x / s)
 
 
-def projection_l2_sphere(x: Any, diam: float = 1.0) -> Any:
-  r"""Projection onto the sphere, { x | || x || = diam}
+def projection_l1_sphere(x: jnp.ndarray, radius: float = 1.0) -> jnp.ndarray:
+  r"""Projection onto the l1 sphere.
 
-  The output is: ``argmin_{y, ||y||= diam} 0.5 ||y - x||^2 = diam * x / ||x||``.
+  The output is:
+    ``argmin_{y, ||y||_1 = radius} ||y - x||``.
 
   Args:
-    x: jnp.ndarray to project.
-    diam: diameter of the sphere.
+    x: array to project.
+    radius: radius of the sphere.
 
   Returns:
-    y: output jnp.ndarray (same size as x) normalized to have suitable norm.
+    y: output array (same shape as x)
   """
-  factor = diam / tree_util.tree_l2_norm(x)
+  return jnp.sign(x) * projection_simplex(jnp.abs(x), radius)
+
+
+def projection_l1_ball(x: jnp.ndarray, radius: float = 1.0) -> jnp.ndarray:
+  r"""Projection onto the l1 ball.
+
+  The output is:
+    ``argmin_{y, ||y||_1 <= radius} ||y - x||``.
+
+  Args:
+    x: array to project.
+    radius: radius of the sphere.
+
+  Returns:
+    y: output array (same structure as x) normalized to have suitable norm.
+  """
+  l1_norm = jax.numpy.linalg.norm(x, ord=1)
+  return jax.lax.cond(l1_norm <= radius,
+                      lambda _: x,
+                      lambda _: projection_l1_sphere(x, radius),
+                      operand=None)
+
+
+def projection_l2_sphere(x: Any, radius: float = 1.0) -> Any:
+  r"""Projection onto the l2 sphere.
+
+  The output is:
+    ``argmin_{y, ||y|| = radius} ||y - x|| = radius * x / ||x||``.
+
+  Args:
+    x: pytree to project.
+    radius: radius of the sphere.
+
+  Returns:
+    y: output pytree (same structure as x) normalized to have suitable norm.
+  """
+  factor = radius / tree_util.tree_l2_norm(x)
   return tree_util.tree_scalar_mul(factor, x)
 
 
-def projection_box_section(x, params, check_feasible=False):
+def projection_l2_ball(x: Any, radius: float = 1.0) -> Any:
+  r"""Projection onto the l2 ball.
+
+  The output is:
+    ``argmin_{y, ||y|| <= radius} ||y - x||``.
+
+  Args:
+    x: pytree to project.
+    radius: radius of the sphere.
+
+  Returns:
+    y: output pytree (same structure as x)
+  """
+  l2_norm = tree_util.tree_l2_norm(x)
+  factor = radius / l2_norm
+  return jax.lax.cond(l2_norm <= radius,
+                      lambda _: x,
+                      lambda _: tree_util.tree_scalar_mul(factor, x),
+                      operand=None)
+
+
+def projection_linf_ball(x: Any, radius: float = 1.0) -> Any:
+  r"""Projection onto the l-infinity ball.
+
+  The output is:
+    ``argmin_{y, ||y||_inf <= radius} ||y - x||``.
+
+  Args:
+    x: pytree to project.
+    radius: radius of the sphere.
+
+  Returns:
+    y: output pytree (same structure as x)
+  """
+  return projection_box(x, (-radius, radius))
+
+
+def projection_hyperplane(x: jnp.ndarray, params: Tuple) -> jnp.ndarray:
+  r"""Projection onto a hyperplane.
+
+  The output is:
+    ``argmin_{y, dot(a, y) = b} ||y - x||``.
+
+  Args:
+    x: pytree to project.
+    params: tuple ``params = (a, b)``, where ``a`` is a vector and ``b`` is a
+      scalar.
+
+  Returns:
+    y: output array (same shape as ``x``)
+  """
+  a, b = params
+  return x - (jnp.dot(a, x) - b) / jnp.dot(a, a) * a
+
+
+def projection_halfspace(x: jnp.ndarray, params: Tuple) -> jnp.ndarray:
+  r"""Projection onto a halfspace.
+
+  The output is:
+    ``argmin_{y, dot(a, y) <= b} ||y - x||``.
+
+  Args:
+    x: pytree to project.
+    params: tuple ``params = (a, b)``, where ``a`` is a vector and ``b`` is a
+      scalar.
+
+  Returns:
+    y: output array (same shape as ``x``)
+  """
+  a, b = params
+  return x - jax.nn.relu(jnp.dot(a, x) - b) / jnp.dot(a, a) * a
+
+
+def projection_affine_set(x: jnp.ndarray, params: Tuple) -> jnp.ndarray:
+  r"""Projection onto an affine set.
+
+  The output is:
+    ``argmin_{y, dot(A, y) = b} ||y - x||``.
+
+  Args:
+    x: pytree to project.
+    params: tuple ``params = (A, b)``, where ``A`` is a matrix and ``b`` is a
+      vector.
+
+  Returns:
+    y: output array (same shape as ``x``)
+  """
+
+  A, b = params
+  solver_fun = quadratic_prog.make_solver_fun()
+  I = jnp.eye(len(x))
+  return solver_fun((I, -x), (A, b))[0]
+
+
+def projection_polyhedron(x: jnp.ndarray, params: Tuple) -> jnp.ndarray:
+  r"""Projection onto a polyhedron.
+
+  The output is:
+    ``argmin_{y, dot(A, y) = b, dot(G, y) <= h} ||y - x||``.
+
+  Args:
+    x: pytree to project.
+    params: tuple ``params = (A, b, G, h)``, where ``A`` is a matrix,
+      ``b`` is a vector, ``G`` is a matrix and ``h`` is a vector.
+
+  Returns:
+    y: output array (same shape as ``x``)
+  """
+
+  A, b, G, h = params
+  solver_fun = quadratic_prog.make_solver_fun()
+  I = jnp.eye(len(x))
+  return solver_fun((I, -x), (A, b), (G, h))[0]
+
+
+def projection_box_section(x: jnp.ndarray,
+                           params: Tuple,
+                           check_feasible: bool = False):
   """Projection onto a box section.
 
   The projection is::
--- tests/projection_test.py
+++ tests/projection_test.py
@@ -18,15 +18,39 @@ import jax
 from jax import test_util as jtu
 import jax.numpy as jnp
 
+from jaxopt import projection
 from jaxopt import quadratic_prog
-from jaxopt.projection import projection_box_section
-from jaxopt.projection import projection_l2_sphere
-from jaxopt.projection import projection_simplex
 
 import numpy as onp
 
 
 class ProjectionTest(jtu.JaxTestCase):
+
+  def test_projection_non_negative(self):
+    x = jnp.array([-1.0, 2.0, 3.0])
+    expected = jnp.array([0, 2.0, 3.0])
+    self.assertArraysEqual(projection.projection_non_negative(x), expected)
+    self.assertArraysEqual(projection.projection_non_negative((x, x)),
+                           (expected, expected))
+
+  def test_projection_box(self):
+    x = jnp.array([-1.0, 2.0, 3.0])
+    expected = jnp.array([0, 2.0, 2.0])
+
+    L, U = 0.0, 2.0
+
+    # lower and upper values are scalars.
+    self.assertArraysEqual(projection.projection_box(x, (L, U)), expected)
+    params = ((L, L), (U, U))
+    self.assertArraysEqual(projection.projection_box((x, x), params),
+                           (expected, expected))
+
+    # lower and upper values are arrays.
+    params = (jnp.ones(len(x)) * L, jnp.ones(len(x)) * U)
+    self.assertArraysEqual(projection.projection_box(x, params), expected)
+    params = ((params[0], params[0]), (params[1], params[1]))
+    self.assertArraysEqual(projection.projection_box((x, x), params),
+                           (expected, expected))
 
   def test_projection_simplex(self):
     rng = onp.random.RandomState(0)
@@ -34,12 +58,12 @@ class ProjectionTest(jtu.JaxTestCase):
     for _ in range(10):
       x = rng.randn(50).astype(onp.float32)
 
-      p = projection_simplex(x)
+      p = projection.projection_simplex(x)
       self.assertAllClose(jnp.sum(p), 1.0)
       self.assertTrue(jnp.all(0 <= p))
       self.assertTrue(jnp.all(p <= 1))
 
-      p = projection_simplex(x, 0.8)
+      p = projection.projection_simplex(x, 0.8)
       self.assertAllClose(jnp.sum(p), 0.8)
       self.assertTrue(jnp.all(0 <= p))
       self.assertTrue(jnp.all(p <= 0.8))
@@ -49,11 +73,11 @@ class ProjectionTest(jtu.JaxTestCase):
     x = rng.rand(5).astype(onp.float32)
     v = rng.randn(5).astype(onp.float32)
 
-    J_rev = jax.jacrev(projection_simplex)(x)
-    J_fwd = jax.jacfwd(projection_simplex)(x)
+    J_rev = jax.jacrev(projection.projection_simplex)(x)
+    J_fwd = jax.jacfwd(projection.projection_simplex)(x)
 
     # Check against theoretical expression.
-    p = projection_simplex(x)
+    p = projection.projection_simplex(x)
     support = (p > 0).astype(jnp.int32)
     cardinality = jnp.count_nonzero(support)
     J_true = jnp.diag(support) - jnp.outer(support, support) / cardinality
@@ -61,11 +85,11 @@ class ProjectionTest(jtu.JaxTestCase):
     self.assertArraysAllClose(J_true, J_rev)
 
     # Check vector Jacobian product.
-    vJ, = jax.vjp(projection_simplex, x)[1](v)
+    vJ, = jax.vjp(projection.projection_simplex, x)[1](v)
     self.assertArraysAllClose(vJ, jnp.dot(v, J_true))
 
     # Check vector Jacobian product.
-    Jv = jax.jvp(projection_simplex, (x,), (v,))[1]
+    Jv = jax.jvp(projection.projection_simplex, (x,), (v,))[1]
     self.assertArraysAllClose(Jv, jnp.dot(J_true, v))
 
   def test_projection_simplex_vmap(self):
@@ -73,19 +97,19 @@ class ProjectionTest(jtu.JaxTestCase):
     X = rng.randn(3, 50).astype(onp.float32)
 
     # Check with default s=1.0.
-    P = jax.vmap(projection_simplex)(X)
+    P = jax.vmap(projection.projection_simplex)(X)
     self.assertArraysAllClose(jnp.sum(P, axis=1), jnp.ones(len(X)))
     self.assertTrue(jnp.all(0 <= P))
     self.assertTrue(jnp.all(P <= 1))
 
     # Check with s=0.8.
-    P = jax.vmap(projection_simplex)(X, 0.8 * jnp.ones(len(X)))
+    P = jax.vmap(projection.projection_simplex)(X, 0.8 * jnp.ones(len(X)))
     self.assertArraysAllClose(jnp.sum(P, axis=1), 0.8 * jnp.ones(len(X)))
     self.assertTrue(jnp.all(0 <= P))
     self.assertTrue(jnp.all(P <= 0.8))
 
   def test_projection_simplex_vmap_diff(self):
-    proj = jax.vmap(projection_simplex)
+    proj = jax.vmap(projection.projection_simplex)
 
     def fun(X):
       return jnp.sum(proj(X) ** 2)
@@ -101,18 +125,146 @@ class ProjectionTest(jtu.JaxTestCase):
     dir_deriv = jnp.vdot(jax.grad(fun)(X), U)
     self.assertAllClose(dir_deriv, dir_deriv_num, atol=1e-3)
 
+  def test_projection_l1_sphere(self):
+    rng = onp.random.RandomState(0)
+
+    x = rng.randn(50).astype(onp.float32)
+
+    p = projection.projection_l1_sphere(x)
+    self.assertAllClose(jnp.sum(jnp.abs(p)), 1.0)
+
+    radius = 3.21
+    p = projection.projection_l1_sphere(x, radius)
+    self.assertAllClose(jnp.sum(jnp.abs(p)), radius)
+
+  def _check_projection_ball(self, ball_fun, sphere_fun, norm, check_pytree):
+    rng = onp.random.RandomState(0)
+
+    x = rng.randn(50).astype(onp.float32)
+    norm_value = jnp.linalg.norm(x, ord=norm)
+
+    # x is already in the ball
+    big_radius = norm_value * 2
+    p = ball_fun(x, big_radius)
+    self.assertArraysAllClose(x, p)
+    J = jax.jacrev(ball_fun)(x, big_radius)
+    self.assertArraysAllClose(J, jnp.eye(len(x)))
+    J = jax.jacrev(ball_fun, argnums=1)(x, big_radius)
+    self.assertArraysAllClose(J, jnp.zeros_like(x))
+
+    # x is at the boundary of the ball
+    p = ball_fun(x, norm_value)
+    self.assertArraysAllClose(x, p)
+    J = jax.jacrev(ball_fun)(x, norm_value)
+    self.assertArraysAllClose(J, jnp.eye(len(x)))
+    J = jax.jacrev(ball_fun, argnums=1)(x, norm_value)
+    self.assertArraysAllClose(J, jnp.zeros_like(x))
+
+    # x is outside of the ball
+    small_radius = norm_value / 2
+    p = ball_fun(x, small_radius)
+    self.assertAllClose(jnp.linalg.norm(p, ord=norm), small_radius)
+    J = jax.jacrev(ball_fun)(x, small_radius)
+    J2 = jax.jacrev(sphere_fun)(x, small_radius)
+    self.assertArraysAllClose(J, J2)
+    J = jax.jacrev(ball_fun, argnums=1)(x, small_radius)
+    J2 = jax.jacrev(sphere_fun, argnums=1)(x, small_radius)
+    self.assertArraysAllClose(J, J2)
+
+    if check_pytree:
+      # x is a pytree
+      pytree = (x[:25], x[25:])
+      p = ball_fun(pytree, norm_value)
+      self.assertArraysAllClose(x[:25], p[0])
+      self.assertArraysAllClose(x[25:], p[1])
+
+  def test_projection_l1_ball(self):
+    self._check_projection_ball(projection.projection_l1_ball,
+                                projection.projection_l1_sphere,
+                                norm=1, check_pytree=False)
+
   def test_projection_l2_sphere(self):
     rng = onp.random.RandomState(0)
 
     x = rng.randn(50).astype(onp.float32)
 
-    p = projection_l2_sphere(x)
+    p = projection.projection_l2_sphere(x)
     self.assertAllClose(jnp.sum(p ** 2), 1.0)
 
-    diam = 3.21
+    radius = 3.21
+    p = projection.projection_l2_sphere(x, radius)
+    self.assertAllClose(jnp.sqrt(jnp.sum(p ** 2)), radius)
 
-    p = projection_l2_sphere(x, diam)
-    self.assertAllClose(jnp.sqrt(jnp.sum(p ** 2)), diam)
+  def test_projection_l2_ball(self):
+    self._check_projection_ball(projection.projection_l2_ball,
+                                projection.projection_l2_sphere,
+                                norm=2, check_pytree=True)
+
+  def test_projection_linf_ball(self):
+    rng = onp.random.RandomState(0)
+
+    x = rng.randn(50).astype(onp.float32)
+    linf_norm = jnp.max(jnp.abs(x))
+
+    p = projection.projection_linf_ball(x, linf_norm)
+    self.assertArraysAllClose(x, p)
+
+    p = projection.projection_linf_ball(x, linf_norm * 0.5)
+    self.assertAllClose(jnp.max(jnp.abs(p)), linf_norm * 0.5)
+
+  def test_projection_hyperplane(self):
+    rng = onp.random.RandomState(0)
+
+    x = rng.randn(50).astype(onp.float32)
+    a = rng.randn(50).astype(onp.float32)
+    b = 1.0
+
+    p = projection.projection_hyperplane(x, (a, b))
+    self.assertAllClose(jnp.dot(a, p), b)
+
+  def test_projection_affine_set(self):
+    rng = onp.random.RandomState(0)
+
+    x = rng.randn(5).astype(onp.float32)
+    A = rng.randn(3, 5).astype(onp.float32)
+    b = rng.randn(3).astype(onp.float32)
+    params = (A, b)
+
+    p = projection.projection_affine_set(x, params)
+    self.assertAllClose(jnp.dot(A, p), b)
+
+    jac_fun = jax.jacrev(projection.projection_affine_set, argnums=0)
+    jac_x = jac_fun(x, params)
+    self.assertEqual(jac_x.shape, (5, 5))
+
+    jac_fun = jax.jacrev(projection.projection_affine_set, argnums=1)
+    jac_A, jac_b = jac_fun(x, params)
+    self.assertEqual(jac_A.shape, (5, 3, 5))
+    self.assertEqual(jac_b.shape, (5, 3))
+
+  def test_projection_polyhedron(self):
+    rng = onp.random.RandomState(0)
+
+    x = rng.randn(5).astype(onp.float32)
+    A = rng.randn(3, 5).astype(onp.float32)
+    b = rng.randn(3).astype(onp.float32)
+    G = rng.randn(2, 5).astype(onp.float32)
+    h = rng.randn(2).astype(onp.float32)
+    params = (A, b, G, h)
+
+    p = projection.projection_polyhedron(x, params)
+    self.assertAllClose(jnp.dot(A, p), b)
+
+    jac_fun = jax.jacrev(projection.projection_polyhedron, argnums=0)
+    jac_x = jac_fun(x, params)
+    self.assertEqual(jac_x.shape, (5, 5))
+
+    jac_fun = jax.jacrev(projection.projection_polyhedron, argnums=1)
+    jac_A, jac_b, jac_G, jac_h = jac_fun(x, params)
+    self.assertEqual(jac_A.shape, (5, 3, 5))
+    self.assertEqual(jac_b.shape, (5, 3))
+    self.assertEqual(jac_G.shape, (5, 2, 5))
+    self.assertEqual(jac_h.shape, (5, 2))
 
   def test_projection_box_section(self):
     x = jnp.array([1.2, 3.0, 0.7])
@@ -121,7 +273,7 @@ class ProjectionTest(jtu.JaxTestCase):
     beta = jnp.ones_like(x) * 0.8
     c = 1.0
     params = (alpha, beta, w, c)
-    p = projection_box_section(x, params)
+    p = projection.projection_box_section(x, params)
     self.assertAllClose(jnp.dot(w, p), c, atol=1e-5)
     self.assertTrue(jnp.all(alpha <= p))
     self.assertTrue(jnp.all(p <= beta))
@@ -142,9 +294,11 @@ class ProjectionTest(jtu.JaxTestCase):
     alpha = jnp.zeros_like(x)
     beta = jnp.ones_like(x)
     params = (alpha + 1.5, beta, w, 1.0)
-    self.assertRaises(ValueError, projection_box_section, x, params, True)
+    self.assertRaises(ValueError, projection.projection_box_section, x, params,
+                      True)
     params = (alpha, beta - 1.5, w, 1.0)
-    self.assertRaises(ValueError, projection_box_section, x, params, True)
+    self.assertRaises(ValueError, projection.projection_box_section, x, params,
+                      True)
 
   def test_projection_box_section_simplex(self):
     rng = onp.random.RandomState(0)
@@ -155,12 +309,12 @@ class ProjectionTest(jtu.JaxTestCase):
     c = 1.0
     params = (alpha, beta, w, c)
 
-    p = projection_simplex(x)
-    p2 = projection_box_section(x, params)
+    p = projection.projection_simplex(x)
+    p2 = projection.projection_box_section(x, params)
     self.assertArraysAllClose(p, p2, atol=1e-4)
 
-    J = jax.jacrev(projection_simplex)(x)
-    J2 = jax.jacrev(projection_box_section)(x, params)
+    J = jax.jacrev(projection.projection_simplex)(x)
+    J2 = jax.jacrev(projection.projection_box_section)(x, params)
     self.assertArraysAllClose(J, J2)
 
 
