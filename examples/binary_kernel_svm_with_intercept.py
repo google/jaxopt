fa356ea5c8a7e19af3850b981aca0926bead23af
Mathieu Blondel <mblondel@google.com>
2021-07-07 17:00:50 +0000

    Add SVM examples.
    PiperOrigin-RevId: 383435235

---
+++ examples/binary_kernel_svm_with_intercept.py
@@ -0,0 +1,95 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Binary kernel SVM with intercept.
+
+The dual objective of binary kernel SVMs with an intercept contains both
+box constraints and an equality constraint, making it challenging to solve.
+The state-of-the-art algorithm to solve this objective is SMO (Sequential
+minimal optimization). We nevertheless demonstrate in this example how to solve
+it by projected gradient descent, by projecting on the constraint set
+using projection_box_section.
+"""
+
+from absl import app
+import jax.numpy as jnp
+from jaxopt import projection
+from jaxopt import prox
+from jaxopt import proximal_gradient2 as proximal_gradient
+import numpy as onp
+from sklearn import datasets
+from sklearn import preprocessing
+from sklearn import svm
+
+
+def objective_fun(beta, lam, K, y):
+  """Dual objective of binary kernel SVMs with intercept."""
+  # The dual objective is:
+  # fun(beta) = 0.5 beta^T K beta - beta^T y
+  # subject to
+  # sum(beta) = 0
+  # 0 <= beta_i <= C if y_i = 1
+  # -C <= beta_i <= 0 if y_i = -1
+  # where C = 1.0 / lam
+  return 0.5 * jnp.dot(beta, jnp.dot(K, beta)) - jnp.dot(beta, y)
+
+
+def binary_kernel_svm_skl(K, y, lam, tol=1e-5):
+  svc = svm.SVC(kernel="precomputed", C=1.0 / lam).fit(K, y)
+  dual_coef = onp.zeros(K.shape[0])
+  dual_coef[svc.support_] = svc.dual_coef_[0]
+  return dual_coef
+
+
+def main(argv):
+  del argv
+
+  # Prepare data.
+  X, y = datasets.make_classification(n_samples=20, n_features=5,
+                                      n_informative=3, n_classes=2,
+                                      random_state=0)
+  X = preprocessing.Normalizer().fit_transform(X)
+  y = y * 2 - 1  # Transform labels from {0, 1} to {-1, 1}.
+  lam = 1.0
+  C = 1./ lam
+  K = jnp.dot(X, X.T)  # Use a linear kernel.
+
+  # Define projection operator.
+  box_lower = jnp.where(y == 1, 0, -C)
+  box_upper = jnp.where(y == 1, C, 0)
+  w = jnp.ones(X.shape[0])
+
+  def proj(beta, hyperparams_proj):
+    # Not used here but all prox and projection operators must accept an
+    # hyperparameter.
+    del hyperparams_proj
+    proj_params = (box_lower, box_upper, w, 0.0)
+    return projection.projection_box_section(beta, proj_params)
+
+  prox_fun = prox.make_prox_from_projection(proj)
+
+  # Run solver.
+  beta_init = jnp.ones(X.shape[0])
+  solver = proximal_gradient.ProximalGradient(fun=objective_fun, prox=prox_fun,
+                                              tol=1e-3, maxiter=500)
+  beta_fit = solver.run(beta_init, hyperparams_prox=None, lam=lam,
+                        K=K, y=y).params
+
+  # Compare the obtained dual coefficients with sklearn.
+  beta_fit_skl = binary_kernel_svm_skl(K, y, lam)
+  print(beta_fit)
+  print(beta_fit_skl)
+
+if __name__ == "__main__":
+  app.run(main)
---
+++ examples/multiclass_linear_svm.py
@@ -0,0 +1,69 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Multiclass linear SVM (without intercept)."""
+
+from absl import app
+import jax.numpy as jnp
+from jaxopt import block_cd2 as block_cd
+from jaxopt import objectives
+from jaxopt import projection
+from jaxopt import prox
+from jaxopt import proximal_gradient2 as proximal_gradient
+import numpy as onp
+from sklearn import datasets
+from sklearn import preprocessing
+from sklearn import svm
+
+
+def multiclass_linear_svm_skl(X, y, lam, tol=1e-5):
+  svc = svm.LinearSVC(loss="hinge", dual=True, multi_class="crammer_singer",
+                      C=1.0 / lam, fit_intercept=False, tol=tol).fit(X, y)
+  return svc.coef_.T
+
+
+def main(argv):
+  del argv
+
+  # Generate data.
+  n_samples, n_classes = 20, 3
+  X, y = datasets.make_classification(n_samples=n_samples, n_features=5,
+                                      n_informative=3, n_classes=n_classes,
+                                      random_state=0)
+  Y = preprocessing.LabelBinarizer().fit_transform(y)
+  Y = jnp.array(Y)
+
+  # Set up parameters.
+  block_prox = prox.make_prox_from_projection(projection.projection_simplex)
+  fun = objectives.multiclass_linear_svm_dual
+  data = (X, Y)
+  lam = 1000.0
+  beta_init = jnp.ones((n_samples, n_classes)) / n_classes
+
+  # Run solver.
+  bcd = block_cd.BlockCoordinateDescent(fun=fun,
+                                        block_prox=block_prox,
+                                        maxiter=3500, tol=1e-5)
+  sol = bcd.run(beta_init, hyperparams_prox=None, lam=lam, data=data)
+
+  # Compare against sklearn.
+  W_skl = multiclass_linear_svm_skl(X, y, lam)
+  W_fit = jnp.dot(X.T, (Y - sol.params)) / lam
+
+  print(W_skl)
+  print(W_fit)
+
+
+if __name__ == "__main__":
+  app.run(main)
