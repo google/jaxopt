63a0d4bfdc72f0336c249fc42f216ff5181f2c12
Mathieu Blondel <mblondel@google.com>
2021-07-13 18:44:21 +0000

    Add setup script.
    PiperOrigin-RevId: 384514786

---
+++ setup.py
@@ -0,0 +1,57 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup script for JAXopt."""
+
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+folder = os.path.dirname(__file__)
+version_path = os.path.join(folder, "jaxopt", "version.py")
+
+__version__ = None
+with open(version_path) as f:
+  exec(f.read(), globals())
+
+req_path = os.path.join(folder, "requirements.txt")
+install_requires = []
+if os.path.exists(req_path):
+  with open(req_path) as fp:
+    install_requires = [line.strip() for line in fp]
+
+setup(
+    name="jaxopt",
+    version=__version__,
+    description="Hardware accelerated, batchable and differentiable optimizers in JAX.",
+    author="Google LLC",
+    author_email="no-reply@google.com",
+    url="https://github.com/google/jaxopt",
+    license="Apache 2.0",
+    packages=find_packages(),
+    package_data={},
+    install_requires=install_requires,
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="optimization, root finding, implicit differentiation, jax",
+    requires_python=">=3.7",
+)
