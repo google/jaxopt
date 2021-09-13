d7e5cc1b12ddeb35ca83da8eb628ff4bd7825dd6
QB3 <quentin.bertrand@inria.fr>
2021-09-13 11:37:42 +0200

    [ciskip] updated benchmark

--- benchmarks/sparse_vjp.py
+++ benchmarks/sparse_vjp.py
@@ -12,21 +12,21 @@ from jaxopt import linear_solve
 from sklearn import datasets
 
 X, y = datasets.make_regression(
-  n_samples=1_000, n_features=10_000, random_state=0)
+  n_samples=100, n_features=100_000, random_state=0)
 
 L = onp.linalg.norm(X, ord=2) ** 2
 
 
-def optimality_fun(params, lam, X, y):
+def optimality_fun(params, X, y, lam):
   return prox.prox_lasso(
     params - X.T @ (X @ params - y) / L, lam * len(y) / L) - params
 
 
 def make_restricted_optimality_fun(support):
-  def restricted_optimality_fun(restricted_params, lam, X, y):
+  def restricted_optimality_fun(restricted_params, X, y, lam):
     # this is suboptimal, I would try to compute restricted_X once for all
     restricted_X = X[:, support]
-    return optimality_fun(restricted_params, lam, restricted_X, y)
+    return optimality_fun(restricted_params, restricted_X, y, lam)
   return restricted_optimality_fun
 
 
@@ -34,14 +34,14 @@ lam_max = jnp.max(jnp.abs(X.T @ y)) / len(y)
 lam = lam_max / 2
 t_start = time.time()
 sol = test_util.lasso_skl(X, y, lam)
-print(sol)
 t_optim = time.time() - t_start
 
+onp.random.seed(0)
 rand = onp.random.normal(0, 1, len(sol))
 dict_times = {}
 dict_grad = {}
 
-for maxiter in [10, 100, 1000, 2_000]:
+for maxiter in [10, 100, 1000, 2000]:
   def solve(matvec, b):
     return linear_solve.solve_normal_cg(
       matvec, b, None, tol=1e-32, maxiter=maxiter)
@@ -49,9 +49,9 @@ for maxiter in [10, 100, 1000, 2_000]:
   vjp = lambda g: idf.root_vjp(
     optimality_fun=optimality_fun,
     sol=sol,
-    args=(lam, X, y),
+    args=(X, y, lam),
     cotangent=g,
-    solve=solve)[0]  # vjp w.r.t. lam
+    solve=solve)[2]  # vjp w.r.t. lam
 
   t_start = time.time()
   grad = vjp(rand)
@@ -69,19 +69,31 @@ vjp_sparse = lambda g: idf.sparse_root_vjp(
   optimality_fun=optimality_fun,
   make_restricted_optimality_fun=make_restricted_optimality_fun,
   sol=sol,
-  args=(lam, X, y),
+  args=(X, y, lam),
   cotangent=g,
-  solve=solve_sparse)[0]  # vjp w.r.t. lam
+  solve=solve_sparse)[2]  # vjp w.r.t. lam
+
+vjp_sparse2 = lambda g: idf.sparse_root_vjp2(
+  optimality_fun=optimality_fun,
+  sol=sol,
+  args=(X, y, lam),
+  cotangent=g,
+  solve=solve_sparse)[2]  # vjp w.r.t. lam
 
 t_start = time.time()
 grad_sparse = vjp_sparse(rand)
 t_jac_sparse = time.time() - t_start
+
+t_start = time.time()
+grad_sparse2 = vjp_sparse(rand)
+t_jac_sparse2 = time.time() - t_start
 
 print("Time taken to solve the Lasso optimization problem %.3f" % t_optim)
 for maxiter in dict_times.keys():
   print("Time taken to compute the gradient with n= %i iterations %.3f | distance to the sparse gradient %.e" % (
     maxiter, dict_times[maxiter], jnp.linalg.norm(dict_grad[maxiter] - grad_sparse) / grad_sparse))
 print("Time taken to compute the gradient with the sparse implementation %.3f" % t_jac_sparse)
+print("Time taken to compute the gradient with the sparse2 implementation %.3f" % t_jac_sparse2)
 
 
 # Computation time are the same, which is very weird to me
