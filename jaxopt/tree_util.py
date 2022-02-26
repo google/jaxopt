36efb908a5abe29c530ffb660ee076391edd8378
fllinares <fllinares@google.com>
2022-02-26 14:50:27 +0000

    Add support for leaf cond pytrees in tree_util.tree_where.

--- jaxopt/tree_util.py
+++ jaxopt/tree_util.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from jaxopt._src.tree_util import broadcast_pytrees
 from jaxopt._src.tree_util import tree_map
 from jaxopt._src.tree_util import tree_multimap
 from jaxopt._src.tree_util import tree_reduce
